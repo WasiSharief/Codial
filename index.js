1598c0cf2e6b46a82d4599118218d78ee12a315e
WasiSharief <wasisharief57@gmail.com>
2020-11-09 21:21:30 +0530

    8th commit: did some changes in ejs views

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const port = 8000;
 const path = require('path');
 
 const app = express();
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
 app.use('/',require('./routes'));
 app.use(express.static('assets'));
+
 app.listen(port,function(err){
     
     if(err){
