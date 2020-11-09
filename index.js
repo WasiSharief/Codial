b6cc5a356f32871029465579a7eaadaac755e2c3
WasiSharief <wasisharief57@gmail.com>
2020-11-09 18:24:48 +0530

    fourth commit: modified for views but not yet finalized

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const port = 8000;
+const path = require('path');
 
 const app = express();
+// app.use('view engine','ejs');
+// app.use('views',path.join(__dirname,'views'));
 app.use('/',require('./routes'));
 
 app.listen(port,function(err){
